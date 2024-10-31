e3684340b0a7b245e53df1efc6e611c860068aa6
HaicoMaters <haicomaters@gmail.com>
2024-10-31 16:24:39 +0000

    Added repository and quey options classes and Get All Async method to prepare for Database Crud operations to be added

---
+++ BasicRestaurantWebsite/BasicRestaurantWebsite/Controllers/IngredientController.cs
@@ -0,0 +1,21 @@
+﻿using BasicRestaurantWebsite.Data;
+using BasicRestaurantWebsite.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BasicRestaurantWebsite.Controllers
+{
+    public class IngredientController : Controller
+    {
+        private Repository<Ingredient> ingredients;
+
+        public IngredientController(ApplicationDbContext context)
+        {
+            ingredients = new Repository<Ingredient>(context);
+        }
+        
+        public async Task<IActionResult> Index()
+        {
+            return View(await ingredients.GetAllAsync());
+        }
+    }
+}
