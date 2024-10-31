7241868bca05a88b4d9ecc28b1e51c74cdb0cfd4
HaicoMaters <haicomaters@gmail.com>
2024-10-31 15:06:27 +0000

    Added table creation

--- BasicRestaurantWebsite/BasicRestaurantWebsite/Data/ApplicationDbContext.cs
+++ BasicRestaurantWebsite/BasicRestaurantWebsite/Data/ApplicationDbContext.cs
@@ -1,13 +1,45 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using BasicRestaurantWebsite.Models;
 
 namespace BasicRestaurantWebsite.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+        }
+
+        // Create Tables
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Ingredient> Ingredients { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+        public DbSet<ProductIngredient> ProductIngredients { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            // Join the Ingredient and Product Tables via the ProductIngredients Tables
+            builder.Entity<ProductIngredient>()
+                .HasKey(pi => new { pi.ProductId, pi.IngredientId}); // Composite primary key combining foreign keys
+
+            // Each product has many ingredients
+            builder.Entity<ProductIngredient>()
+                .HasOne(pi => pi.Product)
+                .WithMany(p => p.ProductIngredients)
+                .HasForeignKey(pi => pi.ProductId);
+
+            builder.Entity<ProductIngredient>()
+                .HasOne(pi => pi.Ingredient)
+                .WithMany(i => i.ProductIngredients)
+                .HasForeignKey(pi => pi.IngredientId);
+
+            //Seed Data
+            
         }
     }
 }
